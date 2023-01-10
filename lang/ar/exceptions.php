00375b205c05628ebea3b2a8db8c3595411122a3
Abdulkarim Omer <a.o.almuhaureq@bootfi.com>
2023-01-10 10:23:40 +0300

    Add langs files

---
+++ lang/ar/exceptions.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'record_not_found' => 'لم يتم العثور على السجل',
+    'route_not_found' => 'لم يتم العثور على المسار', // 'API route not found',
+    'page_not_found' => 'لم يتم العثور على الصفحة',
+    'bad_request' => 'طلب خاطئ',
+    'login_required' => 'يجب عليك تسجيل الدخول',
+    'forbidden' => 'أنت غير مصرح لهذا الإجراء',
+    'internal_server_error' => 'Internal server error.',
+    'must_be_approved' => 'يجب ان تكون حالة المنشاة (مقبول) لاتمام هذا العملية',
+    'must_be_new' => 'يجب ان تكون حالة المنشاة (جديد) لاتمام هذا العملية',
+    'must_be_rejected' => 'يجب ان تكون حالة المنشاة (مرفوض) لاتمام هذه العملية',
+    'file_not_found' => 'لم يتم العثور على الملف',
+    'prevent_job_deletion' => 'لا يمكن حذف هذه الوظيفة لأنها تحتوي على مرشحين',
+    'invitation_already_accepted' => 'لقد تم قبول الدعوة مسبقاً',
+    'invitation_must_be_accepted' => 'يجب أن يتم قبول حالة الدعوة لإكمال العملية',
+    'invitation_already_been_sent_to_the_pipeline' => 'تم إرسال المرشح بالفعل إلى خط الأنابيب',
+    'employment_form_must_be_submitted' => 'يجب أن تكون حالة نموذج التوظيف مقدمة لإكمال العملية'
+];
---
+++ lang/en/exceptions.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'record_not_found' => 'Record not found',
+    'route_not_found' => 'API route not found',
+    'page_not_found' => 'Page not found',
+    'bad_request' => 'Bad request',
+    'login_required' => 'You have to login',
+    'forbidden' => 'You are unauthorized for this action',
+    'internal_server_error' => 'Internal server error.',
+    'must_be_approved' => 'The status of facility must be approved to continue this process',
+    'must_be_new' => 'The status of facility must be new to continue this process',
+    'must_be_rejected' => 'The status of facility must be rejected to continue this process',
+    'file_not_found' => 'File not found',
+    'prevent_job_deletion' => 'This job cannot be deleted because it contains a candidates',
+    'invitation_already_accepted' => 'The invitation has already been accepted',
+    'invitation_must_be_accepted' => 'Invitation status must be accepted to complete the process',
+    'invitation_already_been_sent_to_the_pipeline' => 'The candidate has already been sent to the pipeline',
+    'employment_form_must_be_submitted' => 'Employment form status must be submitted to complete the process',
+];
