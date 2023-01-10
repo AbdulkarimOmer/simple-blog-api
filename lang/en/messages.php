7fb98099f1a2e7d3acfbb6d455ea02988d8a07ec
Abdulkarim Omer <a.o.almuhaureq@bootfi.com>
2023-01-10 17:32:28 +0300

    Add response messages

---
+++ lang/en/messages.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'get_data' => 'Resource fetched successfully.',
+    'create_data' => 'Resource created successfully.',
+    'update_data' => 'Resource updated successfully.',
+    'delete_data' => 'Resource deleted successfully.',
+    'not_found_data' => 'Element not found please go back to main page.',
+    'validation_error' => 'The given data was invalid.',
+];
