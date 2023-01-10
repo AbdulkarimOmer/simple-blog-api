23a90fd7f8dd349af190f871c77311d1847d3633
Abdulkarim Omer <a.o.almuhaureq@bootfi.com>
2023-01-10 10:36:41 +0300

    Made update in users table and model

--- Modules/User/Database/Migrations/2014_10_12_000000_create_users_table.php
+++ Modules/User/Database/Migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,19 @@ return new class extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->snowflake()->primary();
+
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+
+            $table->timestamp('email_verified_at')->nullable();
+            $table->boolean('status')->default(false);
+
             $table->rememberToken();
+            
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
--- Modules/User/Entities/User.php
+++ Modules/User/Entities/User.php
@@ -4,13 +4,15 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Snowflake\Snowflakes;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable,Snowflakes,SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -39,6 +41,7 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
+        'id' => SnowflakeCast::class,
         'email_verified_at' => 'datetime',
     ];
 }
